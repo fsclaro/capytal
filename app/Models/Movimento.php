af4a7338d48dce6b6c6710959c3068505f1a2c0d
Nando Salles <fsclaro@gmail.com>
2024-04-29 15:24:25 -0300

    Model e Migration de Movimento

---
+++ app/Models/Movimento.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Movimento extends Model
+{
+    use HasFactory;
+
+    protected $table = 'movimentos';
+
+    protected $fillable = [
+        'user_id',
+        'conta_id',
+        'categoria_id',
+        'tipo_documento_id',
+        'descricao',
+        'dt_vencto',
+        'vl_vencto',
+        'dt_pagto',
+        'vl_pagto',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'dt_vencto' => 'datetime:d/m/Y',
+            'dt_pagto' => 'datetime:d/m/Y',
+            'created_at' => 'datetime:d/m/Y H:i:s',
+            'updated_at' => 'datetime:d/m/Y H:i:s',
+            'deleted_at' => 'datetime:d/m/Y H:i:s'
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categoria(): BelongsTo
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function conta(): BelongsTo
+    {
+        return $this->belongsTo(Conta::class);
+    }
+
+    public function tipoDocumento(): BelongsTo
+    {
+        return $this->belongsTo(TipoDocumento::class);
+    }
+}
---
+++ database/migrations/2024_04_29_150000_create_movimentos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movimentos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('conta_id')->constrained();
+            $table->foreignId('categoria_id')->constrained();
+            $table->foreignId('tipo_documento_id')->constrained();
+            $table->string('descricao');
+            $table->date('dt_vencto');
+            $table->decimal('vl_vencto',10,2);
+            $table->date('dt_pagto')->nullable();
+            $table->decimal('vl_pagto',10,2)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movimentos');
+    }
+};
